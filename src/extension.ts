8ae948fbbf9e42e7090ba421047611c5a25554d3
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2017-03-21 21:59:51 +0900

    add runR

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,15 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { workspace, window, commands, ExtensionContext, StatusBarAlignment, TextEditor, Disposable } from 'vscode';
+import cp = require('child_process');
+
+let willSaveTextDocument: Disposable;
+let configurationChangedListener: Disposable;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
@@ -14,14 +18,37 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    function createRterm() {
+        window.showInformationMessage('createRterm');
+    }
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    function runR()  {
+        let path = window.activeTextEditor.document.fileName;
+        let cmd = workspace.getConfiguration('rtvsc').get('rtvsc.rterm.windows', "C:\\Program Files\\Microsoft\\MRO\\R-3.2.5\\bin\\x64\\Rterm.exe");
+        let args = ["--no-restore",
+		"--no-save",
+		"--quiet",
+        "--file=" + path];
+        cp.execFile(cmd, args, {}, (err, stdout, stderr) => {
+            try {
+                if (err) {
+                    console.log(err);
+                }
+                let lines = stdout.toString().split('\n');
+                window.showInformationMessage(stdout)
+            } catch (e) {
+                window.showInformationMessage(e.msg);
+            }
+        });
 
-    context.subscriptions.push(disposable);
+        
+        // window.showInformationMessage('runR');
+    }
+
+    context.subscriptions.push(
+        commands.registerCommand('rtvsc.createRterm', createRterm),
+        commands.registerCommand('rtvsc.runR', runR)
+    );
 }
 
 // this method is called when your extension is deactivated
