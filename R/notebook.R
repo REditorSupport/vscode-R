3d2c61a5e404e43a30287858265f26699a3daae4
Kun Ren <renkun@outlook.com>
2020-08-12 23:31:51 +0800

    Update R/notebook.R

--- R/notebook.R
+++ R/notebook.R
@@ -9,22 +9,28 @@ local({
 
   ls.str(env)
   while (TRUE) {
-    cat("Waiting\n")
-    con <- socketConnection(host = "127.0.0.1", port = env$port,
+    con <- try(socketConnection(host = "127.0.0.1", port = env$port,
       blocking = TRUE, server = TRUE,
-      open = "r+")
-    line <- readLines(con, n = 1)
-    request <- jsonlite::fromJSON(line)
-    cat(sprintf("[%s] %s\n",
-      request$time, request$expr))
-    expr <- parse(text = request$expr)
-    res <- try(eval(expr, globalenv()), silent = TRUE)
-    str <- list(
-      type = if (inherits(res, "try-error")) "error" else "output",
-      result = utils::capture.output(print(res))
-    )
-    result <- jsonlite::toJSON(str, auto_unbox = TRUE, force = TRUE)
-    writeLines(result, con)
-    close(con)
+      open = "r+"), silent = TRUE)
+    if (inherits(con, "try-error")) {
+      message(con)
+    } else {
+      tryCatch({
+        line <- readLines(con, n = 1)
+        request <- jsonlite::fromJSON(line)
+        cat(sprintf("[%s]\n%s\n",
+          request$time, request$expr))
+        expr <- parse(text = request$expr)
+        res <- try(eval(expr, globalenv()), silent = TRUE)
+        str <- list(
+          type = if (inherits(res, "try-error")) "error" else "output",
+          result = utils::capture.output(print(res))
+        )
+        result <- jsonlite::toJSON(str, auto_unbox = TRUE, force = TRUE)
+        writeLines(result, con)
+      }, error = function(e) {
+        message(e)
+      }, finally = close(con))
+    }
   }
 })
