dd6a8a116442aa8d0250602833fbac2acee59fe1
Miles McBain <miles.mcbain@gmail.com>
2020-09-08 23:38:38 +1000

    deploy vsc-r-api files in extension dir

--- src/session.ts
+++ src/session.ts
@@ -49,6 +49,10 @@ export function deploySessionWatcher(extensionPath: string) {
     fs.copySync(path.join(extensionPath, 'R', 'init.R'), path.join(watcherDir, 'init.R'));
     console.info('[deploySessionWatcher] Deploy .Rprofile');
     fs.copySync(path.join(extensionPath, 'R', '.Rprofile'), path.join(watcherDir, '.Rprofile'));
+    console.info('[deploySessionWatcher] Deploy vsc_r_api_util.R');
+    fs.copySync(path.join(extensionPath, 'R', 'vsc_r_api_util.R'), path.join(watcherDir, 'vsc_r_api_util.R'));
+    console.info('[deploySessionWatcher] Deploy vsc_r_api.R');
+    fs.copySync(path.join(extensionPath, 'R', 'vsc_r_api.R'), path.join(watcherDir, 'vsc_r_api.R'));
     console.info('[deploySessionWatcher] Done');
 }
 
