4147a96a945404a5c19a8848c2a168a627a50652
Miles McBain <miles.mcbain@gmail.com>
2020-10-13 20:11:18 +1000

    quarantine rstuidoapi args in request response

--- R/init.R
+++ R/init.R
@@ -420,7 +420,7 @@ if (interactive() &&
       }
 
       request_response <- function(command, ...) {
-        request(command, ..., sd__ = dir_session)
+        request(command, args = list(...), sd = dir_session)
         wait_start <- Sys.time()
         while (!get_response_lock()) {
           if ((Sys.time() - wait_start) > response_timeout)
