2e90ca20934461e5dc7a27209721e72e2f2e6823
Miles McBain <miles.mcbain@gmail.com>
2020-10-15 14:33:49 +1000

    fix runTextInTerm call after rebase

--- src/rstudioapi.ts
+++ src/rstudioapi.ts
@@ -317,7 +317,7 @@ export async function launchAddinPicker() {
     await window.showQuickPick<AddinItem>(getAddinPickerItems(), addinPickerOptions);
 
   if (!(typeof addinSelection === 'undefined')) {
-    await runTextInTerm(activeRTerm, addinSelection.package + ':::' + addinSelection.binding + '()');
+    await runTextInTerm(addinSelection.package + ':::' + addinSelection.binding + '()');
   }
 }
 
