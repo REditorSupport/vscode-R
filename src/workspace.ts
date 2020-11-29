e8e79f821e8d3d1c3d6121bb0ecbd9fd4b50026a
ElianHugh <elianhte@gmail.com>
2020-11-30 01:08:08 +1100

    Workspace.ts refactor
    Removed superflous code, changed import, changed formatting

--- src/workspace.ts
+++ src/workspace.ts
@@ -1,4 +1,4 @@
-import * as vscode from 'vscode';
+import { TreeDataProvider, EventEmitter, TreeItemCollapsibleState, TreeItem, ProviderResult, Event } from 'vscode';
 import { globalenv } from './session';
 
 export interface WorkspaceAttr {
@@ -10,15 +10,11 @@ export interface WorkspaceAttr {
     }
 }
 
-const workspace: WorkspaceAttr = globalenv;
-
-export class WorkspaceDataProvider implements vscode.TreeDataProvider<WorkspaceItem> {
-	private _onDidChangeTreeData: vscode.EventEmitter<
-		WorkspaceItem | undefined | null | void
-	> = new vscode.EventEmitter<WorkspaceItem | undefined | null | void>();
-	readonly onDidChangeTreeData: vscode.Event<
-		WorkspaceItem | undefined | null | void
-	> = this._onDidChangeTreeData.event;
+export class WorkspaceDataProvider implements TreeDataProvider<WorkspaceItem> {
+	private _onDidChangeTreeData: EventEmitter<WorkspaceItem | undefined | null | void>
+		= new EventEmitter<WorkspaceItem | undefined | null | void>();
+	readonly onDidChangeTreeData: Event<WorkspaceItem | undefined | null | void>
+		= this._onDidChangeTreeData.event;
 
 	refresh(): void {
 		this.data = globalenv;
@@ -27,21 +23,15 @@ export class WorkspaceDataProvider implements vscode.TreeDataProvider<WorkspaceI
 
 	data: WorkspaceAttr;
 
-	constructor() {
-		this.data = workspace;
-		console.log(this.data);
-	}
+	constructor() {}
 
-	getTreeItem(
-		element: WorkspaceItem
-	): vscode.TreeItem | Thenable<vscode.TreeItem> {
+	getTreeItem(element: WorkspaceItem): TreeItem | Thenable<TreeItem> {
 		return element;
 	}
 
-	getChildren(
-		element?: WorkspaceItem
-	): vscode.ProviderResult<WorkspaceItem[]> {
-		return Promise.resolve(this.getWorkspaceItems(this.data));
+	getChildren(element?: WorkspaceItem): ProviderResult<WorkspaceItem[]> {
+		return Promise.resolve(
+			this.getWorkspaceItems(this.data));
 	}
 
 	private getWorkspaceItems(data: any): WorkspaceItem[] {
@@ -58,26 +48,31 @@ export class WorkspaceDataProvider implements vscode.TreeDataProvider<WorkspaceI
 				type,
 				typeDetailed,
 				length,
-				vscode.TreeItemCollapsibleState.None
+				TreeItemCollapsibleState.None
 			);
 		};
 
-		const items = data
-			? Object.keys(data).map((item) =>
-					toItem(item, data[item].class, data[item].type, data[item].str, data[item].length)
-			  ) : [];
+		const items = data ? Object.keys(data).map((key) =>
+			toItem(
+				key,
+				data[key].class,
+				data[key].type,
+				data[key].str,
+				data[key].length
+			)) : [];
+
 		return items;
 	}
 }
 
-export class WorkspaceItem extends vscode.TreeItem {
+export class WorkspaceItem extends TreeItem {
 	constructor(
 		label: string,
 		rClass: string[],
 		type: string,
 		typeDetailed: string,
 		length: number,
-		collapsibleState: vscode.TreeItemCollapsibleState
+		collapsibleState: TreeItemCollapsibleState
 	) {
 		super(label, collapsibleState);
 		this.description = typeDetailed;
