fce87e34fa3b91af65a70a843cf005f15afa4630
Andrew Craig <andy.peter.craig@gmail.com>
2021-04-04 11:54:54 +0900

    Add task Check

--- src/extension.ts
+++ src/extension.ts
@@ -144,6 +144,8 @@ export async function activate(context: vscode.ExtensionContext): Promise<apiImp
     vscode.tasks.registerTaskProvider(type, {
         provideTasks() {
             return [
+                new vscode.Task({type: type}, vscode.TaskScope.Workspace, 'Check', 'R',
+                    new vscode.ShellExecution('Rscript -e "devtools::check()"')),
                 new vscode.Task({type: type}, vscode.TaskScope.Workspace, 'Document', 'R',
                     new vscode.ShellExecution('Rscript -e "devtools::document()"')),
                 new vscode.Task({type: type}, vscode.TaskScope.Workspace, 'Install', 'R',
