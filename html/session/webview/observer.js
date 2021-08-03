3b4717c90b87b43aa9442fa4e14cef784f8af768
Elian H. Thiele-Evans <60372411+ElianHugh@users.noreply.github.com>
2021-08-03 13:14:12 +1000

    Only test specific tags
    - No need to test tags that do not typically have src values. This can be expanded if a tag is missing from the list

--- html/session/webview/observer.js
+++ html/session/webview/observer.js
@@ -1,12 +1,18 @@
 const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 const replaceReg = /vscode-webview:\/\//;
 const testReg = /vscode-webview:\/\/.*\.[A-Za-z/0-9_-]*?\/.+/;
+const watchedTags = [
+  "IMG",
+  "A",
+  "LINK",
+  "SCRIPT"
+]
 
 const observer = new MutationObserver(mutations => {
   const len = mutations.length;
   for (let i = 0; i < len; i++) {
     const targ = mutations[i].target;
-    if (testReg.test(targ.src)) {
+    if (watchedTags.includes(targ.tagName) && testReg.test(targ.src)) {
       const newSrc = targ.src.replace(replaceReg, 'https://');
       console.log(
         `%c[VSC-R] %cThe file request '${targ.src}' was converted to the URL '${newSrc}'. Reason: the request appears to refer to a URL, not a local file as suggested by the file scheme. %cIf you believe this to be in error, please log an issue on GitHub.`,
