a6d16fc9f3dd5351f201a3595fc1cc27936cf52e
ElianHugh <elianhte@gmail.com>
2021-08-06 02:27:01 +1000

    Update getSettings.R

--- R/getSettings.R
+++ R/getSettings.R
@@ -1,16 +1,29 @@
-fileCon <- file("~/.config/Code/User/settings.json")
+fileCon <- if (file.exists("~/.config/Code/User/settings.json")) {
+    # windows
+    file("~/.config/Code/User/settings.json")
+} else if (file.exists("~/Library/Application Support/Code/User/settings.json")) {
+    # mac
+    file("~/.config/Code/User/settings.json")
+} else if (file.exists("~/.config/Code/User/settings.json")) {
+    # linux
+    file("~/.config/Code/User/settings.json")
+} else if (file.exists(paste0("/mnt///c/Users/", Sys.getenv("USER"), "/AppData/Roaming/Code/User/settings.json"))) {
+    # WSL
+    file(paste0("/mnt///c/Users/", Sys.getenv("USER"), "/AppData/Roaming/Code/User/settings.json"))
+} else {
+    NULL
+}
+
+
 vsc_settings <- jsonlite::fromJSON(paste(readLines(fileCon), collapse = ""))
 ops <- vsc_settings[grep("r.rOptions", names(vsc_settings))]$r.rOptions
 
 
-.vsc.setOption <- function(x) {
+lapply(names(ops), function(x) {
     val <- ops[[x]]
-    switch(
-        x,
+    switch(x,
         "vsc.plot" = options("vsc.plot" = val),
         "vsc.globalenv" = options("vsc.globalenv" = val)
         # etc.
     )
-}
-
-lapply(names(ops), .vsc.setOption)
+})
