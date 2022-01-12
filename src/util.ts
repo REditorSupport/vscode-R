2b78757872a363b94dc5b5d7b449c1aa8e7f1c88
Kun Ren <renkun@outlook.com>
2022-01-12 08:25:07 +0000

    Remove tree-kill

--- src/util.ts
+++ src/util.ts
@@ -8,7 +8,6 @@ import * as vscode from 'vscode';
 import * as cp from 'child_process';
 import { rGuestService, isGuestSession } from './liveShare';
 import { extensionContext } from './extension';
-import * as kill from 'tree-kill';
 
 export function config(): vscode.WorkspaceConfiguration {
     return vscode.workspace.getConfiguration('r');
