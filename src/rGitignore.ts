8b1cb587a82f6da4c70b38a439957c65c1f947d2
eitsupi <50911393+eitsupi@users.noreply.github.com>
2022-01-28 09:14:11 +0900

    update the .gitignore file for R (#949)
    * update the .gitignore file for R
    
    * Ask if the .gitignore file should be overwritten if it already exists
    
    * Separate the R.gitignore file
    
    * minor fix
    
    * fix
    
    * Handle multi-root workspace
    
    Co-authored-by: Kun Ren <renkun@outlook.com>

--- src/rGitignore.ts
+++ src/rGitignore.ts
@@ -1,33 +1,33 @@
 'use strict';
 
 import { writeFile } from 'fs-extra';
+import { existsSync, readFileSync } from 'fs';
 import { join } from 'path';
-import { window, workspace } from 'vscode';
-// From "https://github.com/github/gitignore/raw/master/R.gitignore"
-const ignoreFiles = ['.Rhistory',
-                     '.Rapp.history',
-                     '.RData',
-                     '*-Ex.R',
-                     '/*.tar.gz',
-                     '/*.Rcheck/',
-                     '.Rproj.user/',
-                     'vignettes/*.html',
-                     'vignettes/*.pdf',
-                     '.httr-oauth',
-                     '/*_cache/',
-                     '/cache/',
-                     '*.utf8.md',
-                     '*.knit.md',
-                     'rsconnect/'].join('\n');
+import { window } from 'vscode';
+import { extensionContext } from './extension';
+import { getCurrentWorkspaceFolder } from './util';
 
-export function createGitignore(): void {
-    if (workspace.workspaceFolders[0].uri.path === undefined) {
-        void window.showWarningMessage('Please open workspace to create .gitignore');
+export async function createGitignore(): Promise<void> {
+    // .gitignore template from "https://github.com/github/gitignore/blob/main/R.gitignore"
+    const ignoreFileTemplate = extensionContext.asAbsolutePath('R/template/R.gitignore');
+    const ignoreFileContent = readFileSync(ignoreFileTemplate);
 
+    const currentWorkspaceFolder = getCurrentWorkspaceFolder()?.uri.fsPath;
+    if (currentWorkspaceFolder === undefined) {
+        void window.showWarningMessage('Please open a workspace to create .gitignore');
         return;
     }
-    const ignorePath = join(workspace.workspaceFolders[0].uri.path, '.gitignore');
-    writeFile(ignorePath, ignoreFiles, (err) => {
+    const ignorePath = join(currentWorkspaceFolder, '.gitignore');
+    if (existsSync(ignorePath)) {
+        const overwrite = await window.showWarningMessage(
+            '".gitignore" file is already exist. Do you want to overwrite?',
+            'Yes', 'No'
+        );
+        if (overwrite === 'No') {
+            return;
+        }
+    }
+    writeFile(ignorePath, ignoreFileContent, (err) => {
         try {
             if (err) {
                 void window.showErrorMessage(err.name);
