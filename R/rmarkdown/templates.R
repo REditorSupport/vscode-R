e2d8746c94e2a96af5e87dd176a03094b82be77a
Kun Ren <renkun@outlook.com>
2022-02-09 13:19:18 +0800

    Add templates.R

---
+++ R/rmarkdown/templates.R
@@ -0,0 +1,28 @@
+requireNamespace("jsonlite")
+requireNamespace("yaml")
+
+pkgs <- .packages(all.available = TRUE)
+templates <- new.env()
+template_dirs <- lapply(pkgs, function(pkg) {
+  dir <- system.file("rmarkdown/templates", package = pkg)
+  if (dir.exists(dir)) {
+    ids <- list.dirs(dir, full.names = FALSE, recursive = FALSE)
+    for (id in ids) {
+      file <- file.path(dir, id, "template.yaml")
+      if (file.exists(file)) {
+        data <- yaml::read_yaml(file)
+        data$id <- id
+        data$package <- pkg
+        templates[[paste0(pkg, "::", id)]] <- data
+      }
+    }
+  }
+})
+
+template_list <- unname(as.list(templates))
+
+lim <- "---vsc---"
+
+json <- jsonlite::toJSON(template_list, auto_unbox = TRUE)
+
+cat(lim, json, lim, "\n", sep = "")
