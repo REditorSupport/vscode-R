442b8251d11d70a7a92f77617423f09eca76415f
Kun Ren <renkun@outlook.com>
2022-02-10 12:52:22 +0800

    Disable eslint no-explicit-any

--- src/rmarkdown/draft.ts
+++ src/rmarkdown/draft.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
