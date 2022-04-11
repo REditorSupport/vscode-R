0a1acafba32db450f656bedaba7278124200236e
Kun Ren <renkun@outlook.com>
2022-04-11 09:38:22 +0800

    Update languageServer.R

--- R/languageServer.R
+++ R/languageServer.R
@@ -1,6 +1,6 @@
 add_lib_paths <- Sys.getenv("R_LSP_LIB_PATHS")
 if (nzchar(add_lib_paths)) {
-  add_lib_paths <- strsplit(add_lib_paths, "\n", fixed = TRUE)[[1]]
+  add_lib_paths <- strsplit(add_lib_paths, "\n", fixed = TRUE)[[1L]]
   .libPaths(c(.libPaths(), add_lib_paths))
 }
 
@@ -11,7 +11,7 @@ if (!requireNamespace("languageserver", quietly = TRUE)) {
 debug <- Sys.getenv("R_LSP_DEBUG")
 port <- Sys.getenv("R_LSP_PORT")
 
-debug <- if (nzchar(debug)) as.logical(debug)
+debug <- if (nzchar(debug)) as.logical(debug) else FALSE
 port <- if (nzchar(port)) as.integer(port) else NULL
 
 languageserver::run(port = port, debug = debug)
