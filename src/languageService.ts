8183ec05a811b2fd0b1b7bac1f3b5cd4d8ba83ca
Kun Ren <renkun@outlook.com>
2022-09-04 20:37:05 +0800

    Update languageService.ts

--- src/languageService.ts
+++ src/languageService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/await-thenable */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
