f1596a7cbfa88d3eb598486e4d0ffc68b7a58da0
Florian Rupprecht <33600480+nx10@users.noreply.github.com>
2022-09-30 16:47:27 +0200

    Clarify extractLinkingTo.R

--- R/cppproperties/extractLinkingTo.R
+++ R/cppproperties/extractLinkingTo.R
@@ -1,8 +1,8 @@
 
 deps <- unname(read.dcf("DESCRIPTION", "LinkingTo")[1, ]) # Read 'LinkingTo' field from description
-deps <- gsub("\\s|\\n|(\\([^\\)]*\\))", "", deps) # Remove all whitespace and line breaks
+deps <- gsub("\\s|\\n|(\\([^\\)]*\\))", "", deps) # Remove all whitespace, line breaks and version constraints
 deps <- strsplit(deps, ",")[[1]] # Split package names
 deps <- vapply(deps, function(pkg) {
     system.file("include", package = pkg)
 }, character(1)) # Lookup include dir
-cat(paste(deps, collapse = "////")) # Collapse for returning
+cat(paste(deps, collapse = "////")) # Collapse for returning (Newline is technically allowed in posix paths)
