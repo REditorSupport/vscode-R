29868c73838fc884547c46360d3409e158190045
ManuelHentschel <53863351+ManuelHentschel@users.noreply.github.com>
2022-11-27 16:20:07 +0100

    Store cmd args

--- R/help/rdToHtml.R
+++ R/help/rdToHtml.R
@@ -7,12 +7,14 @@
 #'   3. version string of the package
 #'   4. root dir of the package
 
-e <- tools::loadPkgRdMacros(base::commandArgs(TRUE)[4])
+args <- base::commandArgs(TRUE)
+
+e <- tools::loadPkgRdMacros(args[4])
 e <- tools::loadRdMacros(file.path(R.home('share'), 'Rd', 'macros', 'system.Rd'), macros = e)
 
 tools::Rd2HTML(
-    base::commandArgs(TRUE)[1],
-    package=base::commandArgs(TRUE)[2:3],
+    args[1],
+    package=args[2:3],
     dynamic=TRUE,
     encoding='utf-8',
     macros=e,
