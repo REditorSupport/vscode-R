a66414b7abd81a26b602a2d770353a2ad0f9bf9d
Kun Ren <renkun@outlook.com>
2023-10-07 14:01:44 +0800

    Resolve conflict (#1433)

--- R/rmarkdown/templates.R
+++ R/rmarkdown/templates.R
@@ -4,19 +4,19 @@ loadNamespace("yaml")
 pkgs <- .packages(all.available = TRUE)
 templates <- new.env()
 template_dirs <- lapply(pkgs, function(pkg) {
-  dir <- system.file("rmarkdown/templates", package = pkg)
-  if (dir.exists(dir)) {
-    ids <- list.dirs(dir, full.names = FALSE, recursive = FALSE)
-    for (id in ids) {
-      file <- file.path(dir, id, "template.yaml")
-      if (file.exists(file)) {
-        data <- yaml::read_yaml(file)
-        data$id <- id
-        data$package <- pkg
-        templates[[paste0(pkg, "::", id)]] <- data
-      }
+    dir <- system.file("rmarkdown/templates", package = pkg)
+    if (dir.exists(dir)) {
+        ids <- list.dirs(dir, full.names = FALSE, recursive = FALSE)
+        for (id in ids) {
+            file <- file.path(dir, id, "template.yaml")
+            if (file.exists(file)) {
+                data <- yaml::read_yaml(file)
+                data$id <- id
+                data$package <- pkg
+                templates[[paste0(pkg, "::", id)]] <- data
+            }
+        }
     }
-  }
 })
 
 template_list <- unname(as.list(templates))
