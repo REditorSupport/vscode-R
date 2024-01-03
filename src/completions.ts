4b3044d20293513fb700bba473cdb5db4e76623a
sorhawell <sorhawell@gmail.com>
2024-01-03 14:02:59 +0100

    forgot finish_expression

--- src/completions.ts
+++ src/completions.ts
@@ -166,27 +166,15 @@ export class LiveCompletionItemProvider implements vscode.CompletionItemProvider
             });
         } else if(trigger === '$' || trigger === '@') {
             const symbolPosition = new vscode.Position(position.line, position.character - 1);
-            console.log("New completion \n sP",symbolPosition, position.line, position.character);
             if (session.server) {
-                
-                const re = /([a-zA-Z0-9._$@ ])+(?<![@$])/;
-                let exprRange = document.getWordRangeAtPosition(symbolPosition, re)?.with({ end: symbolPosition });
-                
-                if (!exprRange) {
-                    console.log("regex failed take all of document until trigger");
-                    const startPosition = new vscode.Position(0, 0);
-                    exprRange = new vscode.Range(startPosition, symbolPosition);
-                }
-
-                console.log("eR",exprRange);  
+                const startPosition = new vscode.Position(0, 0);
+                const exprRange = new vscode.Range(startPosition, symbolPosition);
                 const expr = document.getText(exprRange);
-                console.log("hello world",expr);
                 const response: RObjectElement[] = await session.sessionRequest(session.server, {
                     type: 'complete',
                     expr: expr,
                     trigger: completionContext.triggerCharacter
                 });
-
                 if (response) {
                     items.push(...getCompletionItemsFromElements(response, '[session]'));
                 }
