4535269a0644a5edd136f081c97c7a16b3a71894
Elian H. Thiele-Evans <60372411+ElianHugh@users.noreply.github.com>
2024-02-15 14:13:23 +1100

    Add current chunk to knit progress

--- src/rmarkdown/manager.ts
+++ src/rmarkdown/manager.ts
@@ -123,7 +123,7 @@ export abstract class RMarkdownManager {
 
                         }
                         const percentRegex = /[0-9]+(?=%)/g;
-                        const divisionRegex = /([0-9]+)\/([0-9]+)/g;
+                        const divisionRegex = /([0-9]+)\/([0-9]+) (\[.*\])/g;
                         const percentRegOutput = dat.match(percentRegex);
                         if (percentRegOutput) {
                             for (const item of percentRegOutput) {
@@ -140,17 +140,16 @@ export abstract class RMarkdownManager {
                             const divisionRegOutput = divisionRegex.exec(dat);
                             if (divisionRegOutput) {
                                 const perc = parseInt(divisionRegOutput[1]) / parseInt(divisionRegOutput[2]) * 100;
+                                const currentChunk = divisionRegOutput[3] ?? '';
                                 progress?.report(
                                     {
                                         increment: perc - currentProgress,
-                                        message: `${Math.ceil(perc)}%`
+                                        message: `${Math.ceil(perc)}% ${currentChunk}`
                                     }
                                 );
                                 currentProgress = perc;
                             }
                         }
-
-
 
                         if (token?.isCancellationRequested) {
                             if (childProcess) {
