97b02131ed1a1d30c9efa049381093f60ec81e75
Randy Lai <randy.cs.lai@gmail.com>
2025-04-20 15:53:05 -0700

    improve R syntax highlight

--- src/test/suite/syntax.test.ts
+++ src/test/suite/syntax.test.ts
@@ -9,31 +9,12 @@ interface syntaxFile {
         'function-declarations': {
             'patterns': [
                 {
-                    'match':string
+                    'begin':string
                 }
             ]
         }
     }
 }
-
-
-/**
- * Converts a POSIX regular expression string into a format
- * that JavaScript can use. This is because vscode parses
- * syntax files using POSIX but JavaScript can't support it.
- * @param {string} s - A string to be used as a regular expression
- */
-const regex_from_posix = function (s: string): string {
-    const mappings = [
-        ['[:alnum:]', 'a-zA-Z0-9'],
-        ['[:alpha:]', 'a-zA-Z']
-    ];
-    let s2 = s;
-    mappings.forEach((el) => {
-        s2 = s2.replace(el[0], el[1]);
-    });
-    return s2;
-};
 
 
 const extension_root: string = join(__dirname, '..', '..', '..');
@@ -43,14 +24,12 @@ console.log(r_syntax_file);
 const rsyntax_raw = readFileSync(r_syntax_file) as unknown;
 const rsyntax: syntaxFile = JSON.parse(rsyntax_raw as string) as syntaxFile;
 
-const function_pattern: string = rsyntax.repository['function-declarations'].patterns[0].match;
-const function_pattern_fixed: string = regex_from_posix(function_pattern);
-
+const function_pattern: string = rsyntax.repository['function-declarations'].patterns[0].begin;
 
 suite('Syntax Highlighting', () => {
 
     test('function-declarations - basic match', () => {
-        const re = new RegExp(function_pattern_fixed);
+        const re = new RegExp(function_pattern);
         const line = 'x <- function(x) {';
         const match = re.exec(line);
         assert.ok(match);
@@ -58,7 +37,7 @@ suite('Syntax Highlighting', () => {
     });
 
     test('function-declarations - extra spacing', () => {
-        const re = new RegExp(function_pattern_fixed);
+        const re = new RegExp(function_pattern);
         const line = 'x <- function  (x) {';
         const match = re.exec(line);
         assert.ok(match);
@@ -66,7 +45,7 @@ suite('Syntax Highlighting', () => {
     });
 
     test('function-declarations - false function', () => {
-        const re = new RegExp(function_pattern_fixed);
+        const re = new RegExp(function_pattern);
         const line = 'x <- functions';
         const match = re.exec(line);
         assert.strictEqual(match, null);
