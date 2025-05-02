9803ae47e5d28f7fed8619a3130d3ff115efda59
Fred Wu <jianyun.wu@health.nsw.gov.au>
2025-05-02 10:26:51 +1000

    uncomment index.ts

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,41 +1,41 @@
-///* eslint-disable @typescript-eslint/ban-ts-comment */
-///* eslint-disable @typescript-eslint/no-unsafe-member-access */
-///* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 
-//import * as path from 'path';
-//import * as Mocha from 'mocha';
-//import { glob } from 'glob';
+import * as path from 'path';
+import * as Mocha from 'mocha';
+import { glob } from 'glob';
 
-//export function run(): Promise<void> {
-//    // Create the mocha test
-//    const mocha = new Mocha({
-//        ui: 'tdd',
-//        color: true
-//    });
+export function run(): Promise<void> {
+    // Create the mocha test
+    const mocha = new Mocha({
+        ui: 'tdd',
+        color: true
+    });
 
-//    const testsRoot = path.resolve(__dirname, '..');
+    const testsRoot = path.resolve(__dirname, '..');
 
-//    return new Promise((c, e) => {
-//        glob('**/**.test.js', { cwd: testsRoot })
-//            .then(files => {
-//                // Add files to the test suite
-//                files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+    return new Promise((c, e) => {
+        glob('**/**.test.js', { cwd: testsRoot })
+            .then(files => {
+                // Add files to the test suite
+                files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-//                try {
-//                    // Run the mocha test
-//                    mocha.run(failures => {
-//                        if (failures > 0) {
-//                            e(new Error(`${failures} tests failed.`));
-//                        } else {
-//                            c();
-//                        }
-//                    });
-//                } catch (err) {
-//                    e(err);
-//                }
-//            })
-//            .catch(err => {
-//                return e(err);
-//            });
-//    });
-//}
+                try {
+                    // Run the mocha test
+                    mocha.run(failures => {
+                        if (failures > 0) {
+                            e(new Error(`${failures} tests failed.`));
+                        } else {
+                            c();
+                        }
+                    });
+                } catch (err) {
+                    e(err);
+                }
+            })
+            .catch(err => {
+                return e(err);
+            });
+    });
+}
